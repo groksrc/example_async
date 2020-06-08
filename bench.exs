8a63442e6bce33e3db1fe32591a7a6175dfff162
Joshua Scott <joshua.scott@gmail.com>
2020-06-08 13:11:29 -0500

    Benchmarking example

--- bench.exs
+++ bench.exs
@@ -1,11 +1,53 @@
-{time, "val_1"} =
+{:ok, r} = Redix.start_link()
+input_file = "data/input.txt"
+
+Redix.command!(r, ["FLUSHALL"])
+
+{time, _} =
   :timer.tc(fn ->
-    for x <- 1..1_000 do
-      :timer.sleep(1)
-      ExampleAsync.Saver.set("key_#{x}", "val_#{x}")
-    end
+    input_file
+    |> File.stream!()
+    |> Enum.each(fn key ->
+      key = String.trim(key)
+      val = String.replace(key, "A", "B")
+      ExampleAsync.Saver.set_call(key, val)
+    end)
 
     ExampleAsync.Saver.get("key_1")
   end)
 
-IO.puts("Took #{time / 1_000} ms")
+IO.puts("set with call: #{time / 1_000} ms")
+
+Redix.command!(r, ["FLUSHALL"])
+
+{time, _} =
+  :timer.tc(fn ->
+    input_file
+    |> File.stream!()
+    |> Enum.each(fn key ->
+      key = String.trim(key)
+      val = String.replace(key, "A", "B")
+      ExampleAsync.Saver.set_cast(key, val)
+    end)
+
+    ExampleAsync.Saver.get("key_1")
+  end)
+
+IO.puts("set with cast: #{time / 1_000} ms")
+
+Redix.command!(r, ["FLUSHALL"])
+
+{time, _} =
+  :timer.tc(fn ->
+    input_file
+    |> File.stream!()
+    |> Enum.each(fn key ->
+      key = String.trim(key)
+      val = String.replace(key, "A", "B")
+      ExampleAsync.Saver.set_call_async(key, val)
+    end)
+
+    ExampleAsync.Saver.get("key_1")
+  end)
+
+IO.puts("set with call + early reply: #{time / 1_000} ms")
--- lib/example_async/saver.ex
+++ lib/example_async/saver.ex
@@ -6,28 +6,48 @@ defmodule ExampleAsync.Saver do
   end
 
   # Set up initial state of the process here
-  def init(initial_data) do
-    {:ok, initial_data}
+  def init(_initial_data) do
+    {:ok, redis} = Redix.start_link()
+    {:ok, redis}
   end
 
   # Public API
-  def set(key, value) do
+  def set_call(key, value) do
     GenServer.call(__MODULE__, {:set, key, value})
   end
 
+  def set_cast(key, value) do
+    GenServer.cast(__MODULE__, {:set, key, value})
+  end
+
+  def set_call_async(key, value) do
+    GenServer.call(__MODULE__, {:set_async, key, value})
+  end
+
   def get(key) do
-    GenServer.call(__MODULE__, {:get, key})
+    GenServer.call(__MODULE__, {:get, key}, :infinity)
+  end
+
+  def handle_call({:set, key, value}, _from, redis) do
+    retval = Redix.command!(redis, ["SET", key, value])
+    {:reply, retval, redis}
+  end
+
+  def handle_call({:set_async, key, value}, from, redis) do
+    GenServer.reply(from, :ok)
+
+    Redix.command!(redis, ["SET", key, value])
+    {:noreply, redis}
+  end
+
+  def handle_call({:get, key}, _from, redis) do
+    retval = Redix.command!(redis, ["GET", key])
+    {:reply, retval, redis}
   end
 
   # GenServer callback for GenServer.call
-  def handle_call({:set, key, value}, _from, state) do
-    # Simulate a 1ms round trip to an external database
-    :timer.sleep(1)
-    state = Map.put(state, key, value)
-    {:reply, :ok, state}
-  end
-
-  def handle_call({:get, key}, _from, state) do
-    {:reply, Map.get(state, key), state}
+  def handle_cast({:set, key, value}, redis) do
+    Redix.command!(redis, ["SET", key, value])
+    {:noreply, redis}
   end
 end
